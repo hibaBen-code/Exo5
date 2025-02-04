f9c56130194a50411ad58185b8de5d8bfdbcdb3e
hibaBen-code <benhiba2002@gmail.com>
2025-02-04 14:52:05 -0500

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,27 @@
 
 
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 
 using namespace std;
-int main()
-{
-    std::cout << "Hello World!\n";
+
+void calculerSalaire(string ficLire, string ficEcrire) {
+	ifstream fichier(ficLire);
+	ofstream fichierEcrire(ficEcrire);
+    if (ficLire.fail()){
+        cout<< "l'ouverture du fichier ne sest pas bien fait";
+    else
+        string nom;
+        double heure, tauxHoraire, salaire;
+        while(!ficLire.eof()){
+            if (heure > 40) {
+            salaire = (40 * tauxHoraire) + ((heure - 40) * (tauxHoraire * 1.5));
+        } else {
+            salaire = heure * tauxHoraire;
+        }
+            
+
 }
 
 
